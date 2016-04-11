4b4c14227ba090c358692ebb75b8036194d2c5df
marioalvazquez <marioal.vazquez@gmail.com>
2016-04-11 00:37:53 -0500

    Se agregó menú mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  
+  $('div#menu').click(function(){
+  $('div#menuTop').toggleClass("hoverBottom");
+  $('div#menuMiddle').toggleClass("hoverTop");
+  $('div#menuBottom').toggleClass("hoverMiddle");
+  $('div#header-menu-open').toggleClass("active");
+    $('div#menu').css("background: #FBFBFB");
+})
 });
